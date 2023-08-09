f314bee3a9ac79a032ecc33e6a90377c53ef6e69
Dmitry Tinkov <mrfredyon@gmail.com>
2023-08-09 21:08:48 +0300

    dz

---
+++ 012.Ternary/17/app.js
@@ -0,0 +1,10 @@
+// Пользователь вводит строку. Вывести первый символ строки (индексы)
+
+let str = "baba"
+
+if (typeof a === 'string'){
+    console.log(str[0])
+}
+else{
+    console.log('введи строку')
+}
