ec60888cdaac7f1f2c65bdfccd6870b750018b5b
Dmitry Tinkov <mrfredyon@gmail.com>
2023-08-25 23:34:19 +0300

    dsf

--- 017.array/14/app.js
+++ 017.array/14/app.js
@@ -5,12 +5,30 @@ let arr = [10, 20, 30, 50, 235, 3000];
 
 let newArr = [];
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i]>=10){
-      console.log(arr[i]);
-  }else if(arr[i]>=100){
-      console.log(arr[i]);
-  }else if(arr[i]>=1000){
-      console.log(arr[i]);
+  if (arr[i] >= 10 && arr[i] <= 99) {
+    if (
+      Math.floor(arr[i] / 10) === 1 ||
+      Math.floor(arr[i] / 10) === 2 ||
+      Math.floor(arr[i] / 10) === 5
+    ) {
+      newArr.push(arr[i]);
+    }
+  } else if (arr[i] >= 100 && arr[i] <= 999) {
+    if (
+        Math.floor(arr[i] / 100) == 1 ||
+        Math.floor(arr[i] / 100) == 2 ||
+        Math.floor(arr[i] / 100) == 5
+      ) {
+        newArr.push(arr[i]);
+      }
+  } else if (arr[i] >= 1000) {
+    if (
+        Math.floor(arr[i] / 1000) == 1 ||
+        Math.floor(arr[i] / 1000) == 2 ||
+        Math.floor(arr[i] / 1000) == 5
+      ) {
+        newArr.push(arr[i]);
+      }
   }
 }
 
