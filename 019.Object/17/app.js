e0359a5625b8ebf2832f203f55e169e5ac9d5d40
Dmitry Tinkov <mrfredyon@gmail.com>
2023-09-05 21:24:05 +0300

    vf

--- 019.Object/17/app.js
+++ 019.Object/17/app.js
@@ -3,18 +3,24 @@
 const arr = ["anna", "masha", "anna", "alla", "anna"];
 const obj = {};
 
-for (let i = 0; i < arr.length; i++) {
-  obj[arr[i]] = [];
-}
-
-for (let i = 0; i < obj.length; i++) {
-  if (obj[arr[i]] == arr[i]) {
-    obj[arr[i]].push(arr[i]);
+for (const el of arr) {
+  if (obj[el]) {
+    obj[el] += 1;
+  } else {
+    obj[el] = 1;
   }
-  console.log(obj[arr[i]]);
 }
-console.log(obj);
 
-for(key in obj){
-    if(obj[key] === )
+const arrNums = [];
+
+for (const key in obj) {
+  arrNums.push(obj[key]);
+}
+
+const max = Math.max(...arrNums);
+
+for (const key in obj) {
+  if (obj[key] === max) {
+    console.log(`${key} повторилось ${max} раз`);
+  }
 }
