c503f51948b478997cce1291b0e2422d32ef6ea1
Dmitry Tinkov <mrfredyon@gmail.com>
2023-09-09 11:51:36 +0300

    vf

--- 020.Function/15/app.js
+++ 020.Function/15/app.js
@@ -4,3 +4,35 @@
 // произведения всех элементов массива. Если результат функции проверки – true,
 // то вызывать новую функцию, возвращающую произведение всех элементов
 // массива
+
+const n = 5;
+
+function doArr(n) {
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(+prompt(""));
+  }
+  return arr;
+}
+
+function isValidNUM(arr) {
+  const res = arr.every(function (el) {
+    return typeof el === "number" ? true : false;
+  });
+  return res;
+}
+
+function multiplyArr(arr) {
+  const bool = isValidNUM(arr);
+  if (bool === true) {
+    return arr.reduce((middle, el) => middle * el, 1);
+  } else {
+    return "error";
+  }
+}
+
+const arr = doArr(n);
+
+const mult = multiplyArr(arr);
+
+console.log(mult);
