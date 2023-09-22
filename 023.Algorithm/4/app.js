f4232e6a409f14a0f5f577c121c9bcc6f824990b
Dmitry Tinkov <mrfredyon@gmail.com>
2023-09-22 12:20:46 +0300

    af

--- 023.Algorithm/4/app.js
+++ 023.Algorithm/4/app.js
@@ -3,23 +3,35 @@
 // для массива [1, 2, 3, 4, 5, 6, 2, 3, 4] функция должна вернуть [1, 2, 3, 4, 5, 6].
 
 
-arr = [1, 2, 3, 4, 5, 6, 2, 3, 4]
+// arr = [1, 2, 3, 4, 5, 6, 2, 3, 4]
 
-function bubblrSort (arr){
-  for (let j = arr.length - 1 ; j > 0; j--){
-     for( let i = 0; i< j; i++){
-       if(arr[i]> arr[i+1]){
-         let temp = arr[i]
-         arr[i] = arr[i+1]
-         arr[i+1] = temp
-       }
-     }
+// function bubblrSort (arr){
+//   for (let j = arr.length - 1 ; j > 0; j--){
+//      for( let i = 0; i< j; i++){
+//        if(arr[i]> arr[i+1]){
+//          let temp = arr[i]
+//          arr[i] = arr[i+1]
+//          arr[i+1] = temp
+//        }
+//      }
+//   }
+//   console.log(arr);
+// }
+
+// bubblrSort(arr)
+
+const arr = [1, 2, 3, 4, 5, 6, 2, 3, 4]
+
+const newArr = []
+
+function sortArr (arr,newArr){
+  for(let i = 0; i<arr.length; i++){
+    if(!newArr.includes(arr[i])){
+      newArr.push(arr[i])
+    }
   }
-  console.log(arr);
 }
 
-bubblrSort(arr)
 
-function sortNum (arr){
-  if ()
-}
+sortArr (arr,newArr)
+console.log(newArr);
