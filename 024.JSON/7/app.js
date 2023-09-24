420368d340e2737098e4c72e4c843c976608b761
Dmitry Tinkov <mrfredyon@gmail.com>
2023-09-24 22:58:08 +0300

    df

--- 024.JSON/7/app.js
+++ 024.JSON/7/app.js
@@ -3,3 +3,28 @@
 // 9119 -> 811181 (9
 // 2 === 81, 1
 // 2=== 1)
+
+const num = "090";
+
+function isValidNumber(num) {
+  if (isNaN(num)) {
+    throw new Error("not a number");
+  }
+}
+
+function inSqrt(num) {
+  try {
+    isValidNumber(num);
+    const arr = num.split("");
+    const res = arr.map((el) => {
+      return el ** 2;
+    });
+    return res.join("");
+  } catch (error) {
+    return error.message;
+  }
+}
+
+const res = inSqrt(num);
+
+console.log(res);
