f7473429a726b8bf11706c8113336ca4a8a2a07a
Dmitry Tinkov <mrfredyon@gmail.com>
2023-10-06 22:52:32 +0300

    new dz

--- 028.EventTarget/11/app.js
+++ 028.EventTarget/11/app.js
@@ -1,6 +1,24 @@
 // *Дана таблица c 1 колонкой. Под таблицей
 // сделайте форму (инпут и кнопка), с помощью
 // которой можно будет добавить нового
-// значение в таблицу. 
+// значение в таблицу.
 
-const table
+const btn = document.querySelector("button");
+
+const tbody = document.querySelector("tbody");
+
+const input = document.querySelector("input");
+
+const arr = [];
+btn.addEventListener("click", function (event) {
+  try {
+    if (!input.value) throw new Error("input is empty");
+    const secondItem = document.createElement("td");
+    const text = document.createTextNode(input.value);
+    secondItem.appendChild(text);
+    arr.push(secondItem);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+});
