8b5a648a597695e2222777ba11055e93adbbacd3
Dmitry Tinkov <mrfredyon@gmail.com>
2023-10-18 21:38:37 +0300

    dz

--- 032.OOP/4/app.js
+++ 032.OOP/4/app.js
@@ -3,27 +3,27 @@
 // элементов внутри класса. Создать функцию для подсчета всех четных чисел
 // массива. Добавить проверки на ввод
 
-// class MathСalculation {
-//   n;
-//   arr = [];
+class MathСalculation {
+  n;
+  arr = [];
 
-//   constructor(n) {
-//     this.n = n;
-//   }
-//   pushArr = () => {
-//     for (let i = 0; i < this.n; i++) {
-//       this.arr[i] = i;
-//     }
-//     return this.arr;
-//   };
-//   getSumArr = () => {
-//     let sum = 0;
-//     for (let i = 0; i < this.arr.length; i++) {
-//       if (this.arr[i] % 2 == 0) sum += this.arr[i];
-//     }
-//     return sum;
-//   };
-// }
+  constructor(n) {
+    this.n = n;
+  }
+  pushArr = () => {
+    for (let i = 0; i < this.n; i++) {
+      this.arr[i] = i;
+    }
+    return this.arr;
+  };
+  getSumArr = () => {
+    let sum = 0;
+    for (let i = 0; i < this.arr.length; i++) {
+      if (this.arr[i] % 2 == 0) sum += this.arr[i];
+    }
+    return sum;
+  };
+}
 
 // const mathcalculation = new MathСalculation(5);
 
