f2a426e30432b9a7fc3550b5a5b3fe850585274e
Dmitry Tinkov <mrfredyon@gmail.com>
2023-10-29 20:01:35 +0300

    28.10

---
+++ 034.Extends/12/app.js
@@ -0,0 +1,25 @@
+// На входе n – количество элементов массива.
+// Далее производится заполнение массива с клавиатуры.
+//  Необходимо создать функцию возвращающую новый массив из элементов,
+//  каждое значение которого имеет вид #name.
+// [“hschool”, “company”] -> [“#hschool”, “#company”]
+
+const arr = [];
+const newArr = [];
+const n = 4;
+
+function getArr() {
+  for (let i = 0; i < n; i++) {
+    arr.push(prompt("input str"));
+  }
+}
+
+function modeArr() {
+  arr.map((el) => {
+    newArr.push(`#${el}`);
+  });
+}
+
+getArr();
+const res = modeArr();
+console.log(newArr);
