0973c51ea761eb50251e3e941a30ea8eb2e29aed
Dmitry Tinkov <mrfredyon@gmail.com>
2023-11-08 18:12:10 +0300

    28.10

--- 032.OOP/8/app.js
+++ 032.OOP/8/app.js
@@ -2,6 +2,26 @@
 // строку и проверяет, является ли она корректным емейлом или нет. Если является
 // - возвращает true, если не является - то false.
 
+// class Validator {
+//   email;
+//   constructor(email) {
+//     this.email = email;
+//   }
+//   isEmail() {
+//     try {
+//       if (!/^[a-zA-Z0-9\_\-\.]+@[a-z]+\.[a-z]+/g.test(this.email))
+//         throw new Error("ошибка ввода почты");
+//       else return true;
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   }
+// }
+
+// const validator = new Validator("mrfredyon@gmail.com");
+// const res = validator.isEmail()
+// console.log(res);
+
 class Validator {
   email;
   constructor(email) {
