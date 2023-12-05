30c2774018c95f3dab0ffa99a687917e04a5fb38
Dmitry Tinkov <mrfredyon@gmail.com>
2023-12-05 23:20:44 +0300

    sdf

---
+++ 050.NodeJS/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+app.get("/contact", (req, res) => {
+  res.send("cd");
+});
+app.get("/about", (req, res) => {
+  res.send("cd");
+});
+app.get("/", (req, res) => {
+  const arr = [
+    1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5, 3, 4, 4, 4, 2, 1,
+  ];
+  const uniq = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniq.includes(arr[i])) uniq.push(arr[i]);
+  }
+  res.send(uniq);
+});
+
+app.listen(3000, () => {
+  console.log("server is started");
+});
