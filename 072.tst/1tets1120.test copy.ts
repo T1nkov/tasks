d87ea99d83c4d96c18f2fb8550ba61e1f2961959
Dmitry Tinkov <mrfredyon@gmail.com>
2024-03-13 23:22:47 +0300

    pыва

--- 072.tst/1tets1120.test copy.ts
+++ 072.tst/1tets1120.test copy.ts
@@ -1,10 +1,38 @@
-// Создайте класс PersonArray, который имеет свойство array (массив объектов
-//     Person) и методы:
-//     getNames(): string[] - возвращает массив имен всех людей.
-//     getAdults(): Person[] - возвращает массив только совершеннолетних людей.
-//     getAverageAge(): number - возвращает средний возраст всех людей.
+// 7. Напишите функцию findPairWithSum(numbers: number[], targetSum: number):
+// [number, number] | null, которая принимает массив чисел и целевую сумму, и
+// возвращает пару чисел из массива, сумма которых равна целевой сумме. Если
+// такая пара не найдена, функция должна возвращать null.
 
 
-class PersonArray {
-    arr = [{name:'Ivan'}]
+
+function findPairWithSum(numbers, targetSum) {
+    try {
+        // let values = [targetSum - numbers[0]];
+
+        for (let i = 0; i < numbers.length; i++) {
+            let firstValue = targetSum - numbers[i]
+
+            let secondValue = targetSum - firstValue
+
+            for (let j = 0; j < numbers.length; j++) {
+                if (numbers[j] == secondValue && numbers[j] == firstValue) {
+                    //   console.log(numbers.includes(firstValue[i]));
+    
+                    return [firstValue, secondValue]
+    
+    
+                } else {
+                    // return null;
+                }
+                
+            }
+        }
+        // return null;
+    } catch (error) {
+        return error.message
+    }
 }
+const res21 = findPairWithSum([1, 2, 3, 4, 5, 6], 11);
+console.log(res);
+
+
